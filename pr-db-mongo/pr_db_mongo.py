1cd3e538582d644cd30fd3685ee48bb99337a97a
kilemensi <kilemensi@gmail.com>
2023-08-17 09:09:36 +0300

    Use URI (mongodb+srv schema://) to connect

--- pr-db-mongo/pr_db_mongo.py
+++ pr-db-mongo/pr_db_mongo.py
@@ -1,18 +1,15 @@
 import subprocess
 import sys
-from urllib.parse import urlsplit
+from urllib.parse import quote_plus, urlsplit
 
 import pymongo
 
 PLUGIN_NAME = "pr-db-mongo"
 
+
 def execute_bash(command):
-    return subprocess.run(
-        command,
-        check=True,
-        capture_output=True,
-        text=True
-    )
+    return subprocess.run(command, check=True, capture_output=True, text=True)
+
 
 def configure_pr_app(app_name, db_url):
     print(f"{PLUGIN_NAME}: configuring '{app_name}' ... ", end="")
@@ -38,14 +35,18 @@ def delete_pr_database(db_name, db_url):
     print(f"{PLUGIN_NAME}: deleting database '{db_name}' ... ", end="")
 
     try:
-        parsed_url = urlsplit(db_url)
-        username = parsed_url.username
-        password = parsed_url.password
-        host = parsed_url.hostname
-        port = parsed_url.port
-        client = pymongo.MongoClient(
-            host=host, port=port, username=username, password=password
-        )
+        split_url = urlsplit(db_url)
+        username = split_url.username
+        password = split_url.password
+        scheme = split_url.scheme
+        hostname = split_url.hostname
+        port = split_url.port
+        query = split_url.query
+        credentials = ":".join([quote_plus(c) for c in (username, password)])
+        host = f"{hostname}:{port}" if port else hostname
+        options = f"/?{query}" if query else ""
+        uri = f"{scheme}://{credentials}@{host}{options}"
+        client = pymongo.MongoClient(uri)
         client.drop_database(db_name)
 
         print("done")
@@ -54,22 +55,23 @@ def delete_pr_database(db_name, db_url):
         print(f"{PLUGIN_NAME}:", e)
         sys.exit(1)
 
+
 if __name__ == "__main__":
     if len(sys.argv) != 3:
         sys.exit(f"{PLUGIN_NAME} <dokku app name> <create|delete> ({sys.argv})")
     action = sys.argv[2].lower()
-    if not action in ["create", "delete"]:
+    if action not in ["create", "delete"]:
         sys.exit(f"{PLUGIN_NAME} <dokku app name> <create|delete>")
     app_name = sys.argv[1]
     app_name_pr_number = app_name.split("-pr-")
     if len(app_name_pr_number) == 2:
         original_app_name = app_name_pr_number[0].strip()
         command = ["dokku", "config:get", original_app_name, "MONGODB_URL"]
-        mongodb_url = execute_bash(command)
+        mongodb_url = execute_bash(command).stdout.strip()
         if mongodb_url:
             if action == "create":
                 configure_pr_app(app_name, mongodb_url)
-            else: # action == "delete":
+            else:  # action == "delete":
                 delete_pr_database(app_name, mongodb_url)
         # else: no MONGODB_URL found, silently skip
     # else: app doesn't follow naming convention, silently skip
