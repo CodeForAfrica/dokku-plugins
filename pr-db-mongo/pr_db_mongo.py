267117c8fb6111fa0a1713689a1e667dad58e479
kilemensi <kilemensi@gmail.com>
2023-08-24 14:04:28 +0300

    Lint

--- pr-db-mongo/pr_db_mongo.py
+++ pr-db-mongo/pr_db_mongo.py
@@ -1,7 +1,6 @@
 import subprocess
 import sys
-from urllib.parse import quote_plus, urlsplit, urlunparse
-import os
+from urllib.parse import quote_plus, urlsplit
 
 PLUGIN_NAME = "pr-db-mongo"
 
@@ -9,7 +8,8 @@ PLUGIN_NAME = "pr-db-mongo"
 def execute_bash(command):
     return subprocess.run(command, check=True, capture_output=True, text=True)
 
-def get_uri(db_url, db_name=''):
+
+def get_uri(db_url, db_name=""):
     split_url = urlsplit(db_url)
     username = split_url.username
     password = split_url.password
@@ -21,6 +21,7 @@ def get_uri(db_url, db_name=''):
     host = f"{hostname}:{port}" if port else hostname
     options = f"/{db_name}?{query}" if query else ""
     return f"{scheme}://{credentials}@{host}{options}"
+
 
 def configure_pr_app(uri, app_name):
     print(f"{PLUGIN_NAME}: configuring '{app_name}' ... ", end="")
@@ -41,23 +42,39 @@ def configure_pr_app(uri, app_name):
         print(f"{PLUGIN_NAME}:", e)
         sys.exit(1)
 
+
 def clone_pr_database(original_db_url, app_name):
     try:
         parsed_url = urlsplit(original_db_url)
-        source = parsed_url.path.lstrip('/')
+        source = parsed_url.path.lstrip("/")
         uri = get_uri(original_db_url)
-        command = ["mongodump", "-vvvvv", f"--archive", f"--uri={uri}", f"--db={source}", "|", "mongorestore", "-vvvvv", f"--uri={uri}", f"--archive", f"--nsFrom={source}.*", f"--nsTo={app_name}.*", f"--nsInclude={source}.*"]
+        command = [
+            "mongodump",
+            "-vvvvv",
+            "--archive",
+            f"--uri={uri}",
+            f"--db={source}",
+            "|",
+            "mongorestore",
+            "-vvvvv",
+            f"--uri={uri}",
+            "--archive",
+            f"--nsFrom={source}.*",
+            f"--nsTo={app_name}.*",
+            f"--nsInclude={source}.*",
+        ]
         execute_bash(command)
     except Exception as e:
         print("failed to clone database")
         print(f"{PLUGIN_NAME}:", e)
         sys.exit(1)
 
+
 def delete_pr_database(mongodb_url, db_name):
     print(f"{PLUGIN_NAME}: deleting database '{db_name}' ... ", end="")
     uri = get_uri(mongodb_url, db_name)
     try:
-        command=["mongo", uri,  "--eval", "db.dropDatabase()"]
+        command = ["mongo", uri, "--eval", "db.dropDatabase()"]
         execute_bash(command)
 
         print("done")
@@ -71,6 +88,7 @@ def setup_pr_app(mongodb_url, app_name):
     uri = get_uri(mongodb_url)
     clone_pr_database(mongodb_url, app_name)
     configure_pr_app(uri, app_name)
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
