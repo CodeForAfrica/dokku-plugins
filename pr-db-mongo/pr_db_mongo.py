7963cbf333ce666c0d8263eb8310b47fd61db284
kilemensi <kilemensi@gmail.com>
2023-08-24 14:34:18 +0300

    Organise code

--- pr-db-mongo/pr_db_mongo.py
+++ pr-db-mongo/pr_db_mongo.py
@@ -20,21 +20,49 @@ def get_uri(db_url, db_name=""):
     credentials = ":".join([quote_plus(c) for c in (username, password)])
     host = f"{hostname}:{port}" if port else hostname
     options = f"/{db_name}?{query}" if query else ""
+
     return f"{scheme}://{credentials}@{host}{options}"
 
 
-def configure_pr_app(uri, app_name):
-    print(f"{PLUGIN_NAME}: configuring '{app_name}' ... ", end="")
-    db_url = get_uri(uri, app_name)
+def configure_pr_app(original_mongodb_url, app_name):
+    mongodb_url = get_uri(original_mongodb_url, app_name)
+    commands = [
+        ["dokku", "config:set", "--no-restart", app_name, f"MONGODB_URL={mongodb_url}"],
+        # TODO: get main domain from original app and modify it
+        ["dokku", "domains:add", app_name, f"{app_name}.dev.codeforafrica.org"],
+        ["dokku", "letsencrypt:enable", app_name],
+    ]
+    for command in commands:
+        execute_bash(command)
+
+
+def clone_pr_database(original_mongodb_url, app_name):
+    split_url = urlsplit(original_mongodb_url)
+    source = split_url.path.lstrip("/")
+    uri = get_uri(original_mongodb_url)
+    command = [
+        "mongodump",
+        "-vvvvv",
+        "--archive",
+        f"--uri={uri}",
+        f"--db={source}",
+        "|",
+        "mongorestore",
+        "-vvvvv",
+        f"--uri={uri}",
+        "--archive",
+        f"--nsFrom={source}.*",
+        f"--nsTo={app_name}.*",
+        f"--nsInclude={source}.*",
+    ]
+    execute_bash(command)
+
+
+def setup_pr_app(original_mongodb_url, app_name):
+    print(f"{PLUGIN_NAME}: setting up '{app_name}' ... ", end="")
     try:
-        commands = [
-            ["dokku", "config:set", "--no-restart", app_name, f"MONGODB_URL={db_url}"],
-            # TODO: get main domain from original app and modify it
-            ["dokku", "domains:add", app_name, f"{app_name}.dev.codeforafrica.org"],
-            ["dokku", "letsencrypt:enable", app_name],
-        ]
-        for command in commands:
-            execute_bash(command)
+        clone_pr_database(original_mongodb_url, app_name)
+        configure_pr_app(original_mongodb_url, app_name)
 
         print("done")
     except Exception as e:
@@ -43,37 +71,10 @@ def configure_pr_app(uri, app_name):
         sys.exit(1)
 
 
-def clone_pr_database(original_db_url, app_name):
-    try:
-        parsed_url = urlsplit(original_db_url)
-        source = parsed_url.path.lstrip("/")
-        uri = get_uri(original_db_url)
-        command = [
-            "mongodump",
-            "-vvvvv",
-            "--archive",
-            f"--uri={uri}",
-            f"--db={source}",
-            "|",
-            "mongorestore",
-            "-vvvvv",
-            f"--uri={uri}",
-            "--archive",
-            f"--nsFrom={source}.*",
-            f"--nsTo={app_name}.*",
-            f"--nsInclude={source}.*",
-        ]
-        execute_bash(command)
-    except Exception as e:
-        print("failed to clone database")
-        print(f"{PLUGIN_NAME}:", e)
-        sys.exit(1)
-
-
-def delete_pr_database(mongodb_url, db_name):
+def delete_pr_database(original_mongodb_url, db_name):
     print(f"{PLUGIN_NAME}: deleting database '{db_name}' ... ", end="")
-    uri = get_uri(mongodb_url, db_name)
     try:
+        uri = get_uri(original_mongodb_url, db_name)
         command = ["mongo", uri, "--eval", "db.dropDatabase()"]
         execute_bash(command)
 
@@ -84,28 +85,22 @@ def delete_pr_database(mongodb_url, db_name):
         sys.exit(1)
 
 
-def setup_pr_app(mongodb_url, app_name):
-    uri = get_uri(mongodb_url)
-    clone_pr_database(mongodb_url, app_name)
-    configure_pr_app(uri, app_name)
-
-
 if __name__ == "__main__":
     if len(sys.argv) != 3:
-        sys.exit(f"{PLUGIN_NAME} <dokku app name> <create|delete> ({sys.argv})")
+        sys.exit(f"{PLUGIN_NAME} <dokku app name> <setup|delete> ({sys.argv})")
     action = sys.argv[2].lower()
     if action not in ["setup", "delete"]:
-        sys.exit(f"{PLUGIN_NAME} <dokku app name> <create|delete>")
+        sys.exit(f"{PLUGIN_NAME} <dokku app name> <setup|delete>")
     app_name = sys.argv[1]
     app_name_pr_number = app_name.split("-pr-")
     if len(app_name_pr_number) == 2:
         original_app_name = app_name_pr_number[0].strip()
         command = ["dokku", "config:get", original_app_name, "MONGODB_URL"]
-        mongodb_url = execute_bash(command).stdout.strip()
-        if mongodb_url:
+        original_mongodb_url = execute_bash(command).stdout.strip()
+        if original_mongodb_url:
             if action == "setup":
-                setup_pr_app(mongodb_url, app_name)
+                setup_pr_app(original_mongodb_url, app_name)
             else:  # action == "delete":
-                delete_pr_database(mongodb_url, db_name=app_name)
+                delete_pr_database(original_mongodb_url, app_name)
         # else: no MONGODB_URL found, silently skip
     # else: app doesn't follow naming convention, silently skip
